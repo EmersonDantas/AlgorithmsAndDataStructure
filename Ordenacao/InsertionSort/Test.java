bd6b600e95872ce30bd4629a2e6845ff02f1fac3
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2018-09-19 19:42:35 -0300

    Merge added

--- Ordenacao/InsertionSort/Test.java
+++ Ordenacao/InsertionSort/Test.java
@@ -2,7 +2,7 @@ public class Test{
     public static void main(String[]args){
         InsertionSort ordenador = new InsertionSort();
 
-        int[] vetorOrdenado = ordenador.insertionSort(new int[] {2,5,7,1,3,4,6});
+        int[] vetorOrdenado = ordenador.insertionSort(new String[]{"a","n","v","c"});
         
         System.out.print("Vetor ordenado = [");
         for(int a: vetorOrdenado){
